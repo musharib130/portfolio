b6caa4334ff421e17495f6853ac4b2ffa7032506
musharib130 <musharibayub@gmail.com>
2026-03-22 08:32:23 +0500

    dvh used in addition to vh for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-[100vh]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
