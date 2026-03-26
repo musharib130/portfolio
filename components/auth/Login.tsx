cfb43b37ba8472e8336176c09d131b73b9746b52
musharib130 <musharibayub@gmail.com>
2026-03-26 15:34:02 +0500

    86ex1xzzf: login page completed

---
+++ components/auth/Login.tsx
@@ -0,0 +1,31 @@
+import { p } from "framer-motion/client";
+import { useState } from "react";
+
+export default function Login() {
+    const [show, setShow] = useState(false);
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("email: ", email, "password: ", password)
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen w-screen paper-texture top-0 left-0">
+                {show ? (
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-start gap-4 p-4 bg-white rounded-md shadow">
+                        <p className="bold text-xl">Login</p>
+                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} className="py-2 px-4"/>
+                        <input type="password" placeholder="Password"  onChange={(e) => setPassword(e.target.value)} value={password} className="py-2 px-4" />
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-md"> Login </button>
+                    </form>
+                ) : (
+                    <p>Error 404</p>
+                )}
+            <div className="h-4 w-4 absolute top-0 right-0" onClick={() => setShow(true)}>
+            </div>
+        </div>    
+    )
+}
